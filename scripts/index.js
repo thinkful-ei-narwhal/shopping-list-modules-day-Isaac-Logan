1085576ba2c72bca19425e6a6a3022fd460fd94d
Isaac Elesia <isaacelesia@gmail.com>
2020-03-23 15:31:08 -0500

    refactored shopping list app

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
+import item from './item.js';
+import store from './store.js';
 import shoppingList from './shopping-list.js';
 
-const main = function () {
-  shoppingList.bindEventListeners();
-  shoppingList.render();
+const main = function() {
+	shoppingList.bindEventListeners();
+	shoppingList.render();
 };
 
 $(main);
